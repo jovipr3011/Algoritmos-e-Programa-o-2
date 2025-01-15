d7bb97cbab8e471adca05e71e6b9dfadf3662d6e
João Vitor <joaoprado3011@gmail.com>
2025-01-15 08:39:24 -0300

    Create calculadora.c

---
+++ Lista 2/Questão 1/calculadora.c
@@ -0,0 +1,23 @@
+#include "calculadora.h"
+#include <stdio.h>
+
+float adicionar(float a, float b) {
+    return a + b;
+}
+
+float subtrair(float a, float b) {
+    return a - b;
+}
+
+float multiplicar(float a, float b) {
+    return a * b;
+}
+
+float dividir(float a, float b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        printf("Erro: Divisao indeterminada.\n");
+        return 0;
+    }
+}
