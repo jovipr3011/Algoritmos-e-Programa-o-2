b09028275a022710d1312bb737f30a976dd92b30
João Vitor <joaoprado3011@gmail.com>
2025-01-15 09:09:14 -0300

    Create alunos.h

---
+++ Lista 2/Questão 2/alunos.h
@@ -0,0 +1,13 @@
+#ifndef ALUNOS_H
+#define ALUNOS_H
+
+typedef struct {
+    char nome[50];
+    float notas[3];
+    float media;
+} Aluno;
+
+void adicionarAluno(Aluno alunos[], int *quantidade);
+void listarAlunos(Aluno alunos[], int quantidade);
+
+#endif
